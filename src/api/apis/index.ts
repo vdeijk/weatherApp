a95f9c094eb8ede54f2ad0095206a792dc2b7ba7
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-10-05 12:31:47 +0200

    changes

---
+++ src/api/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ErrorApi';
+export * from './FestivalApi';
+export * from './WeatherApi';
---
+++ src/api/models/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './FestivalDto';
+export * from './ProblemDetails';
+export * from './WeatherDataDto';
